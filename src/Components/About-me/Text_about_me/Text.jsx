15e658e7bfcad08e4702c7b53d9470fc8acd76dd
Nick <shuminskiy.nik@mail.ru>
2021-08-27 20:34:23 +0300

    Refactoring , @media

--- src/Components/About-me/Text_about_me/Text.jsx
+++ src/Components/About-me/Text_about_me/Text.jsx
@@ -4,19 +4,30 @@ import {Title} from "../../Common/Title/Title";
 
 export const Text = () => {
     return (
-        <div>
+        <div className={style.container} >
             <Title title={`I'm Nick Shym`} />
             <p>A Front-end Developer</p>
-            <p>About ME</p>
-          <div style={style.listDiv}>
-              <ol className={style.pills}>
-                  <li>Элемент списка</li>
-                  <li>Элемент списка</li>
-                  <li>Элемент списка</li>
-                  <li>Элемент списка</li>
-                  <li>Элемент списка</li>
-              </ol>
-          </div>
+            <p>Lorem Ipsum From USA is simply dummy text of the printing and type setting industry. Lorem Ipsum has been the standard dummy text ever since.
+                Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has been the industry's standard dummy text ever since
+                the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              <ul  className={style.list4b}>
+                  <li>Core technology stack</li>
+                  <li>React</li>
+                  <li>Redux</li>
+                  <li>Java-script</li>
+                  <li>TypeScript</li>
+                  <li>Core technology stack</li>
+                  <li>React</li>
+                  <li>Redux</li>
+                  <li>Java-script</li>
+                  <li>TypeScript</li>
+                  <li>Core technology stack</li>
+                  <li>React</li>
+                  <li>Redux</li>
+                  <li>Java-script</li>
+                  <li>TypeScript</li>
+              </ul>
+
         </div>
     );
 };
